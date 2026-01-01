288c730fd69126cb7ee07a5e8075649908369355
KoushikPanda1729 <programmer747767@gmail.com>
2026-01-01 14:37:50 +0530

    change name

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -3,7 +3,7 @@ import winston from "winston";
 const logger = winston.createLogger({
     level: "info",
     defaultMeta: {
-        seviceName: "auth-service",
+        seviceName: "catalog-service",
     },
     transports: [
         new winston.transports.File({
